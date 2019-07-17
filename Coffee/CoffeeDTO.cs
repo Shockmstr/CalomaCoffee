044c10cd32f6896cc8d173a1d1f850f24928ae00
duongtaitoan <52193983+duongtaitoan@users.noreply.github.com>
2019-07-17 23:38:06 +0700

    Add files via upload

---
+++ Coffee/CoffeeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee
+{
+    public class CoffeeDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
