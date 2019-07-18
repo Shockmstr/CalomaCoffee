b004daf6d392bf683500ca3b3ab2bd6c8c99ff59
Xuan Thanh <51129107+Bitumm@users.noreply.github.com>
2019-07-18 21:04:49 +0700

    Add files via upload

---
+++ MainCoffee.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CoffeeProject
+{
+    partial class MainCoffee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Size = new System.Drawing.Size(557, 391);
+            this.splitContainer1.SplitterDistance = 385;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainCoffee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 391);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainCoffee";
+            this.Text = "MainCoffee";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ RegisterForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using User;
+
+namespace CoffeeProject
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+    
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string fullname = txtFullname.Text;
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string confirmPass = txtConfirm.Text;
+            string email = txtEmail.Text;
+            string validate = "";
+
+            if (string.IsNullOrWhiteSpace(fullname)) validate += "Full Name\n";
+            if (string.IsNullOrWhiteSpace(username)) validate += "Username\n";
+            if (string.IsNullOrWhiteSpace(password)) validate += "Password\n";
+            if (string.IsNullOrWhiteSpace(confirmPass)) validate += "Confirm Password\n";
+            if (string.IsNullOrWhiteSpace(email)) validate += "Email\n";
+
+            if (validate.Equals(""))
+            {
+                if (confirmPass.Equals(password))
+                {
+                    try
+                    {
+                        UserDAO dao = new UserDAO();
+                        UserDTO dto = new UserDTO { Fullname = fullname, Username = username, Password = password, Email = email, Role = "customer" };
+                        if (dao.CreateUser(dto))
+                        {
+                            MessageBox.Show("Create success!", "Success");
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Create failed!", "Success");
+                        } // end create
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.Message);
+                        if (ex.Message.Contains("PRIMARY KEY"))
+                        {
+                            MessageBox.Show("Username has existed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            txtUsername.Focus();
+                        }
+                        if (ex.Message.Contains("Email"))
+                        {
+                            MessageBox.Show("Email has existed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            txtEmail.Focus();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Confirm password does not match!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtConfirm.Focus();
+                } // end confirm
+            }
+            else
+            {
+                MessageBox.Show("Please fill in following textboxes:\n\n" + validate, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } // end validate
+        }
+    }
+}
